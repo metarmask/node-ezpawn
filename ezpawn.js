1b9e0c7b97c1aa473c8bfbf4782184745e24ff3e
metarmask <ihvarfner@gmail.com>
2019-10-15 20:25:23 +0200

    Refactor
    - Remove semicolons
    - Refactor `spawn` into smaller chunks
    - ...

--- ezpawn.js
+++ ezpawn.js
@@ -1,69 +1,80 @@
-const ChildProcess = require("child_process");
+const ChildProcess = require("child_process")
 
 class SpawnError extends Error {
 	constructor(message, io) {
-		super(message + " (stderr:) " + (io.err ? io.err.toString() : ""));
-		Object.assign(this, io);
+		super(`${message} (stderr:) ${io.err ? io.err.toString() : ""}`)
+		Object.assign(this, io)
 	}
 }
 
 class TimeoutError extends SpawnError {
 	constructor(io) {
-		super("Child timed out", io);
+		super("Child timed out", io)
 	}
 }
 
 class SignalExit extends SpawnError {
 	constructor(signal, io) {
-		super("Child received " + signal, io);
-		this.signal = signal;
+		super(`Child received ${signal}`, io)
+		this.signal = signal
 	}
 }
 
 class NonZeroExit extends SpawnError {
 	constructor(code, io) {
-		if(code === 0) throw new RangeError("NonZeroExit is for non-zero exit codes");
-		super("Child exited with non-zero " + code, io);
-		this.code = code;
+		if(code === 0) throw new RangeError("Exit code 0 is not an error")
+		super(`Child exited with non-zero ${code}`, io)
+		this.code = code
 	}
 }
 
 class EZpawn {
-	static async spawn(command, args, inOptions = {}) {
-		return new Promise((resolve, reject) => {
-const options = Object.assign({
-	stdio: "pipe",
-}, inOptions);
-const process = ChildProcess.spawn(command, args, inOptions);
-const pipes = {};
-for(const [ioName, processKey] of [["out", "stdout"], ["err", "stderr"]]) {
-	const pipe = process[processKey];
-	const collect = {length: 0, buffers: []};
-	pipe.on("data", buffer => {
-		collect.length += buffer.length;
-		collect.buffers.push(buffer);
-	});
-	pipes[ioName] = collect;
-}
-process.on("exit", (code, signal) => {
-	const pipesConcat = {};
-	for(const [ioName, collect] of Object.entries(pipes)) {
-		pipesConcat[ioName] = Buffer.concat(collect.buffers, collect.length);
-	}
-	let error;
-	if(signal) {
-		error = new SignalExit(signal, pipesConcat);
-	} else if(code !== 0) {
-		error = new NonZeroExit(code, pipesConcat);
-	}
-	if(error) {
-		return reject(error);
-	}
-	return resolve(pipesConcat);
-});
-		});
-	}
-}
-Object.assign(EZpawn, {SpawnError, TimeoutError, SignalExit, NonZeroExit});
 
-module.exports = EZpawn;
+static async spawn(...args) {
+	return new Promise((resolve, reject) => {
+		new EZpawn(resolve, reject, args)
+	})
+}
+
+constructor(resolve, reject, spawnArgs) {
+	this.resolve = resolve
+	this.reject = reject
+	const [command, args, inOptions = {}] = spawnArgs
+	const options = Object.assign({
+		stdio: "pipe"
+	}, inOptions)
+	this.process = ChildProcess.spawn(command, args, options)
+	this.process.on("exit", this.onExit.bind(this))
+	this.setupStreams()
+}
+
+setupStreams() {
+	this.streams = {}
+	for(const longName of EZpawn.streamNames) {
+		const shortName = longName.substring("std".length)
+		const stream = {length: 0, buffers: []}
+		this.streams[shortName] = stream
+		this.process[longName].on("data", buffer => {
+			stream.length += buffer.length
+			stream.buffers.push(buffer)
+		})
+	}
+}
+
+onExit(code, signal) {
+	const streamsOut = {}
+	for(const [name, stream] of Object.entries(this.streams)) {
+		streamsOut[name] = Buffer.concat(stream.buffers, stream.length)
+	}
+	if(signal) return this.reject(new SignalExit(signal, streamsOut))
+	if(code !== 0) return this.reject(new NonZeroExit(code, streamsOut))
+	return this.resolve(streamsOut)
+}
+
+}
+Object.defineProperties(EZpawn, {
+	streamNames: {value: ["stdout", "stderr"]}
+})
+Object.assign(EZpawn, {SpawnError, TimeoutError, SignalExit, NonZeroExit})
+
+module.exports = EZpawn
